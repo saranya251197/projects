a83b10fbeaf142bb200ae216aed3e7ace1963bcf
saranya251197 <saranyaramalingam251197@gmail.com>
2020-08-02 20:44:55 +0530

    instagramclone made by angular

---
+++ src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: true,
+  firebase :{
+    apiKey: "AIzaSyCgNMWvmO5z09prqjbZ8JMgXsHi5FIbtIE",
+    authDomain: "react30-1bdfa.firebaseapp.com",
+    databaseURL: "https://react30-1bdfa.firebaseio.com",
+    projectId: "react30-1bdfa",
+    storageBucket: "react30-1bdfa.appspot.com",
+    messagingSenderId: "410422488809",
+    appId: "1:410422488809:web:941b6e5b93db327e504a78"
+  },
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
